a255f0255076b123f7d1c36629da8c75f30ed5c4
Wictor Nogueira <wictorgoesnogueira@gmail.com>
2021-05-15 20:35:25 -0300

    rename dist files

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ A mod that adds Discord Rich Presence integration to Need for Speed: Underground
 ## Installation
 
 1. Install ASI loader.
-2. Get the latest version [here](https://github.com/wictornogueira/nfsu2-rpc/releases/latest/download/discord-rpc.zip).
+2. Get the latest version [here](https://github.com/wictornogueira/nfsu2-rpc/releases/latest/download/nfsu2-rpc.zip).
 3. Extract all files to your game directory.
--- build.ps1
+++ build.ps1
@@ -3,14 +3,14 @@ New-Item -ItemType Directory -Force -Path "build\scripts" | Out-Null
 New-Item -ItemType Directory -Force -Path "dist\" | Out-Null
 
 # Compile
-& i686-w64-mingw32-g++.exe -static-libgcc -mthreads -I src\ -I include\ -L lib\ -shared -o build\scripts\discord-rpc.asi src\discord_rpc.cpp -l discord-rpc
+& i686-w64-mingw32-g++.exe -static-libgcc -mthreads -I src\ -I include\ -L lib\ -l discord-rpc -shared -o build\scripts\nfsu2-rpc.asi src\discord_rpc.cpp
 
 if (!$LastExitCode) {
   # Copy discord-rpc.dll
   Copy-Item "lib\discord-rpc.dll" -Destination "build\"
 
   # Zip stuff
-  Compress-Archive -Force -Path "build\*" -DestinationPath "dist\discord-rpc.zip"
+  Compress-Archive -Force -Path "build\*" -DestinationPath "dist\nfsu2-rpc.zip"
 
   Write-Output "Built successfully."
   exit
