9a23115aa5c70961fe9dea7470f3ea99dd5779aa
Wictor Nogueira <wictorgoesnogueira@gmail.com>
2021-05-22 22:52:42 -0300

    beef build script

--- build.ps1
+++ build.ps1
@@ -1,18 +1,40 @@
-# Create directories
+# Define optional flags
+param (
+  [switch] $l,  # Launch game
+  [switch] $d,  # Generate dist file
+  [switch] $c   # Perform clean build
+)
+
+# Delete old stuff
+if ($c) {
+  Remove-Item -Force -Recurse -Path "build"
+  Remove-Item -Force -Recurse -Path "dist"
+}
+
+# Create build directory
 New-Item -ItemType Directory -Force -Path "build\scripts" | Out-Null
-New-Item -ItemType Directory -Force -Path "dist\" | Out-Null
 
 # Compile
 & i686-w64-mingw32-g++.exe -static-libgcc -mthreads -I src\ -I include\ -L lib\ -l discord-rpc -shared -o build\scripts\nfsu2-rpc.asi src\discord_rpc.cpp
 
 if (!$LastExitCode) {
-  # Copy discord-rpc.dll
-  Copy-Item "lib\discord-rpc.dll" -Destination "build\"
-
-  # Zip stuff
-  Compress-Archive -Force -Path "build\*" -DestinationPath "dist\nfsu2-rpc.zip"
-
   Write-Output "Built successfully."
+
+  # Copy discord-rpc.dll
+  Copy-Item -Force "lib\discord-rpc.dll" -Destination "build\"
+
+  # Create dist file
+  if ($d) {
+    New-Item -ItemType Directory -Force -Path "dist\" | Out-Null
+    Compress-Archive -Force -Path "build\*" -DestinationPath "dist\nfsu2-rpc.zip"
+  }
+
+  # Copy files and launch game
+  if ($l -and $env:NFSU2) {
+    Copy-Item -Recurse -Force "build\*" -Destination "$env:NFSU2"
+    Start-Process -WorkingDirectory "$env:NFSU2" -FilePath "$env:NFSU2\SPEED2.exe"
+  }
+
   exit
 }
 
