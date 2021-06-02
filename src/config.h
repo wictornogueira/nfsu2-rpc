665663dfd9f4305550cb0e27a8a52e6cb22bc594
Wictor Nogueira <wictorgoesnogueira@gmail.com>
2021-06-02 02:45:06 -0300

    Replace prf name with current gm and track

--- src/config.h
+++ src/config.h
@@ -1,5 +1,18 @@
 #define APP_ID "811339054011777065"
 #define IMG_KEY "logo"
 #define IMG_TXT "Need for Speed: Underground 2"
-#define UNK_CAR "Unknown"
 #define UPD_INTVL 5000
+
+#define UNK_CAR "Unknown"
+
+#define IN_MENU "Main Menu" // ANY menu
+#define IN_LOBBY "Lan Lobby"
+
+#define GM_URL "URL"
+#define GM_DRAG "Drag"
+#define GM_DRIFT "Drift"
+#define GM_SPRINT "Sprint"
+#define GM_CIRCUIT "Circuit"
+#define GM_UNKNOWN "Unknown"
+#define GM_STREET_X "Street X"
+#define GM_EXPLORER "Explorer Mode"
--- src/discord_rpc.cpp
+++ src/discord_rpc.cpp
@@ -11,7 +11,7 @@ using namespace utils;
 
 static DWORD WINAPI ThreadEntry (LPVOID lpParam) {
   char state[64];
-  char details[16];
+  char details[64];
   
   Discord_Initialize(APP_ID, 0, 0, 0);
 
@@ -26,10 +26,46 @@ static DWORD WINAPI ThreadEntry (LPVOID lpParam) {
   discord_presence.details = details;
 
   while (1) {
-    (*CAR_PTR < 0)  ? sprintf_s(state, 64, "$%d", *MONEY_PTR)
-                    : sprintf_s(state, 64, "%s - $%d", *CAR_PTR > 30 ? UNK_CAR : CAR_TABLE[*CAR_PTR] , *MONEY_PTR);
+    switch (*C_TRACK_PTR) {
+      case 0:
+      case 2000:
+        sprintf_s(details, 64, (*LBBY_NAME_PTR == 0) ? IN_MENU : IN_LOBBY);
+        break;
+      case 4000:
+        sprintf_s(details, 64, GM_EXPLORER);
+        break;
+      default: {
+        const char* mode;
 
-    memcpy_s(details, 16, PROFILE_PTR, 16);
+        switch (*C_TRACK_PTR / 100) {
+          case 40:
+            mode = GM_CIRCUIT;
+            break;
+          case 41:
+            mode = (*C_TRACK_PTR >= 4174) ? GM_DRIFT : GM_SPRINT;
+            break;
+          case 43:
+            mode = GM_DRIFT;
+            break;
+          case 42:
+          case 44:
+            mode = GM_DRAG;
+            break;
+          case 46:
+            mode = GM_STREET_X;
+            break;
+          case 47:
+            mode = GM_URL;
+            break;
+          default:
+            mode = GM_UNKNOWN;
+        }
+
+        sprintf_s(details, 64, (*LBBY_NAME_PTR == 0) ? "%s - %s" : "%s - %s (LAN)", mode, TRACK_TABLE[*C_TRACK_PTR]);
+      }
+    }
+    
+    sprintf_s(state, 64, (*C_CAR_PTR < 0) ? "" : "%s", *C_CAR_PTR > 30 ? UNK_CAR : CAR_TABLE[*C_CAR_PTR]);
 
     Discord_UpdatePresence(&discord_presence);
     Discord_RunCallbacks();
